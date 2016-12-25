fe48bb538aff156867b53b7d02c4c3d86315a81d
Dmytro.Rastsvietalov <Dmytro.Rastsvietalov@infopulse.com>
2016-12-25 18:18:02 +0200

    changes

--- Rastsvietalov_Sport/Program.cs
+++ Rastsvietalov_Sport/Program.cs
@@ -16,7 +16,7 @@ namespace Rastsvietalov_Sport
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
